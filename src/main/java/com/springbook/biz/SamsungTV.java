c4dca8bf77b271f988f7550567d5a0f5e5c39f2b
Wandu <info10229@gmail.com>
2020-08-26 21:15:56 +0900

    add init-method, destroy-method, lazy-init

--- src/main/java/com/springbook/biz/SamsungTV.java
+++ src/main/java/com/springbook/biz/SamsungTV.java
@@ -5,6 +5,13 @@ public class SamsungTV implements TV{
 		System.out.println("===>Samsung TV 객체 생성");
 	}
 	
+	public void initMethod() {
+		System.out.println("객체 초기화 작업 처리...");
+	}
+	public void destroyMethod() {
+		System.out.println("객체 삭제 전 처리할 로직 처리...");
+	}
+	
 	public void powerOn() {
 		System.out.println("SamsungTV--- 전원 켬");
 	}
