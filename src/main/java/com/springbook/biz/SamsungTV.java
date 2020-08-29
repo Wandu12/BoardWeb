25b87e3a26c111c6df69ff2a9bee9d5b9c56b68f
Wandu12 <info10229@gmail.com>
2020-08-29 23:32:16 +0900

    Add SonySpeaker

--- src/main/java/com/springbook/biz/SamsungTV.java
+++ src/main/java/com/springbook/biz/SamsungTV.java
@@ -1,17 +1,19 @@
 package com.springbook.biz;
 
 public class SamsungTV implements TV{
+	private SonySpeaker speaker;
+	
 	public SamsungTV() {
 		System.out.println("===>Samsung TV 객체 생성");
 	}
-	
+/*	
 	public void initMethod() {
 		System.out.println("객체 초기화 작업 처리...");
 	}
 	public void destroyMethod() {
 		System.out.println("객체 삭제 전 처리할 로직 처리...");
 	}
-	
+	*/
 	public void powerOn() {
 		System.out.println("SamsungTV--- 전원 켬");
 	}
@@ -19,9 +21,11 @@ public class SamsungTV implements TV{
 		System.out.println("SamsungTV--- 전원 끔");
 	}
 	public void volumeUp() {
-		System.out.println("SamsungTV--- 소리 키움");
+		speaker = new SonySpeaker();
+		speaker.volumeUp();
 	}
 	public void volumeDown() {
-		System.out.println("SamsungTV--- 소리 줄임");
+		speaker = new SonySpeaker();
+		speaker.volumeDown();
 	}
 }
