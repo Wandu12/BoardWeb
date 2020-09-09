f8a75a82b9ef0441c289d4877d04d864548f3d24
Wandu <info10229@gmail.com>
2020-09-09 18:30:44 +0900

    add Board VO

---
+++ src/main/java/com/springbook/board/BoardVO.java
@@ -0,0 +1,39 @@
+package com.springbook.board;
+
+import java.sql.Date;
+
+public class BoardVO {
+	private int seq;
+	private String title;
+	private String writer;
+	private String content;
+	private Date regDate;
+	private int cnt;
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public void setRegDate(Date regDate) {
+		this.regDate = regDate;
+	}
+	public void setCnt(int cnt) {
+		this.cnt = cnt;
+	}
+	
+	@Override
+	public String toString() {
+		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
+				+ regDate + ", cnt=" + cnt + "]";
+	}
+	
+	
+	
+}
