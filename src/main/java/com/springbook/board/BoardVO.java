4d59c6c7f5e5faf4574a0f1e0939457fc2c19b2e
Wandu <info10229@gmail.com>
2020-09-09 18:33:59 +0900

    add BoardVO

--- src/main/java/com/springbook/board/BoardVO.java
+++ src/main/java/com/springbook/board/BoardVO.java
@@ -29,6 +29,7 @@ public class BoardVO {
 	}
 	
 	@Override
+	
 	public String toString() {
 		return "BoardVO [seq=" + seq + ", title=" + title + ", writer=" + writer + ", content=" + content + ", regDate="
 				+ regDate + ", cnt=" + cnt + "]";
